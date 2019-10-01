a3026d7e9790fd884341dcb2ea7b398e913c5166
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-01 10:49:04 +0530

    Add files via upload

---
+++ TWClient.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+public class TWClient
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			Socket s=new Socket("localhost",2020);
+			DataInputStream in=new DataInputStream(s.getInputStream());
+			DataOutputStream out=new DataOutputStream(s.getOutputStream());
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str;
+			while(true)
+			{
+				str=br.readLine();
+				out.writeUTF(str);
+				str=in.readUTF();
+				System.out.println(str);
+			}
+		
+		}
+		catch(Exception e)
+		{}
+	}
+}
