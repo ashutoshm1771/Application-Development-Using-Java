af1e9c94e18673bbec809849bca0624196ea5ad7
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-28 22:38:48 +0530

    card

---
+++ CardDemo.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class CardDemo extends JFrame implements ActionListener
+{
+	CardLayout card;
+	Container c;
+	CardDemo()
+	{
+		 card=new CardLayout(40,30);
+		 c=getContentPane();
+		c.setLayout(card);
+		JButton b1=new JButton("Hello");
+		JButton b2=new JButton("World");
+		JButton b3=new JButton("By Ashu");
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		c.add("a",b1);
+		c.add("b",b2);
+		c.add("c",b3);
+		
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		card.next(c);
+	}
+	public static void main(String args[])
+	{
+		CardDemo cd=new CardDemo();
+		cd.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		cd.setSize(300,300);
+		cd.setVisible(true);
+	}
+}
