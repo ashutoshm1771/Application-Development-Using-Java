9a205136d3ad23446d3ba1ac71032f4a566a8343
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-28 22:38:01 +0530

    border

---
+++ BorderLayoutDemo.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class BorderLayoutDemo extends JFrame{
+	public BorderLayoutDemo()
+	{
+		JPanel p=new JPanel();
+		p.setLayout(new BorderLayout());
+		p.add(new JButton("North"),BorderLayout.NORTH);
+		p.add(new JButton("South"),BorderLayout.SOUTH);
+		p.add(new JButton("East"),BorderLayout.EAST);
+		add(p);
+		setSize(300,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public static void main(String args[])
+	{
+		new BorderLayoutDemo();
+	}
+}
