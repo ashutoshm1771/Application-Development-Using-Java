ff1af2eb9db7875a373ac8a482a7dd9c841646ca
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-28 22:34:49 +0530

    modif

--- TWClient.java
+++ TWClient.java
@@ -1,26 +1,27 @@
 import java.io.*;
 import java.net.*;
-public class TWClient
+public class twclient
 {
-	public static void main(String args[])
-	{
-		try
-		{
-			Socket s=new Socket("localhost",2020);
-			DataInputStream in=new DataInputStream(s.getInputStream());
-			DataOutputStream out=new DataOutputStream(s.getOutputStream());
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			String str;
-			while(true)
-			{
-				str=br.readLine();
-				out.writeUTF(str);
-				str=in.readUTF();
-				System.out.println(str);
-			}
-		
-		}
-		catch(Exception e)
-		{}
-	}
+public static void main(String[] args)
+{
+try
+{
+Socket s=new Socket("localhost",2020);
+DataInputStream in=new DataInputStream(s.getInputStream());
+DataOutputStream out=new DataOutputStream(s.getOutputStream());
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str;
+while(true)
+{
+str=br.readLine();
+out.writeUTF(str);
+str=in.readUTF();
+System.out.println(str);
+}
+}
+catch(Exception e)
+{
+}
+}
+
 }
--- TWServer.java
+++ TWServer.java
@@ -1,27 +1,28 @@
 import java.io.*;
 import java.net.*;
-public class TWServer
+public class twserver
 {
-	public static void main(String args[])
-	{
-		try
-		{
-			ServerSocket ss=new ServerSocket(2020);
-			Socket s=ss.accept();
-			DataInputStream in=new DataInputStream(s.getInputStream());
-			DataOutputStream out=new DataOutputStream(s.getOutputStream());
-			BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-			String str;
-			while(true)
-			{
-				str=in.readUTF();
-				System.out.println(str);
-				str=br.readLine();
-				out.writeUTF(str);
-			}
-		}
-		catch(Exception e)
-		{}
-		
-	}
+public static void main(String[] args)
+{
+try
+{
+ServerSocket ss=new ServerSocket(2020);
+Socket s=ss.accept();
+DataInputStream in=new DataInputStream(s.getInputStream());
+DataOutputStream out=new DataOutputStream(s.getOutputStream());
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str;
+while(true)
+{
+str=br.readLine();
+out.writeUTF(str);
+str=in.readUTF();
+System.out.println(str);
+}
+}
+catch(Exception e)
+{
+}
+}
+
 }
